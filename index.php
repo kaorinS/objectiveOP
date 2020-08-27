b6a5395f4face80681a1dbba0a4bba4eec73dac9
KAORI <tanewaltz@yahoo.co.jp>
2020-08-27 13:54:04 +0900

    オブジェクト指向部 OP作成開始！

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>あっちむいてホイ</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet">
+    <!-- font awesome -->
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- スタート画面 -->
+        <!-- <div class="main main-color">
+            <div class="title-container">
+                <h1 class="h1 title">あっちむいてホイ</h1>
+            </div>
+            <form class="start-container" method="post">
+                <input type="submit" class="startbutton" name="start" value="はじめる">
+            </form>
+        </div> -->
+        <!-- ゲームスタート -->
+        <div class="game-top">
+            <div class="game-main main-color">
+                <div class="enemy-box">
+                    <div class="enemy-box-1">
+                        <div class="enemy-name-box">
+                            <span class="name enemy-name">おかあさん</span>
+                        </div>
+                        <div class="enemy-img-box">
+                            <img src="image/obasan03_smile.png" class="enemy-img">
+                        </div>
+                        <div class="enemy-life-box">
+                            <span class="enemy-life -h"><i class="fas fa-heart"></i></span>
+                            <span class="enemy-life -h"><i class="fas fa-heart"></i></span>
+                            <span class="enemy-life -b"><i class="fas fa-heart-broken"></i></span>
+                        </div>
+                    </div>
+                    <div class="enemy-box-2">
+                        <div class="enemy-comment-box">
+                            <img src="image/comment5.png" class="enemy-comment-img">
+                            <div class="enemy-comment-box2">
+                                <span class="enemy-comment">うふふ♡</span>
+                            </div>
+                        </div>
+                        <div class="enemy-action-box">
+                            <!-- じゃんけん -->
+                            <!-- <img src="image/j-choki.png" alt="" class="enemy-action-img"> -->
+                            <div class="enemy-hoi-box">
+                                <span class="enemy-hoi"><i class="fas fa-arrow-alt-circle-up"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="game-sub">
+                <div class="sub-comment-start">
+                    <span class="sub-num">1</span>かいせんめ<br>
+                    <span class="sub-enemy-name">おかあさん</span>と<br>
+                    しょうぶ！！
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
